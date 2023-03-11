535f3eb347bc3736789d682bb4efaa1f76927f49
ansalr <ansalr.email@gmail.com>
2023-03-11 22:46:43 +0530

    add two string

--- firstproject/calc/views.py
+++ firstproject/calc/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def home (request):
     return render(request,'home.html',{'name':'ansal'})
+
+
+def add(request):
+
+    val1 = request.GET['num1']
+    val2 = request.GET['num2']
+    res = val1 + val2
+
+    return render (request,"result.html",{'result':res})
