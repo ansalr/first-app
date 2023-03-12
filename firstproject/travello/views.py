4f0446699a680a8bbe18213b8d1427d63fb312e1
ansalr <ansalr.email@gmail.com>
2023-03-12 21:24:42 +0530

    edit destination details

--- firstproject/travello/views.py
+++ firstproject/travello/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from.models import Destination
 
 # Create your views here.
 
 def index (request):
-    return render(request,'index.html',{'prive': 700})
+
+    dest1 = Destination()
+    dest1.name = "hawai"
+    dest1.desc = " beach resort"
+    dest1.price = 666
+
+    dest2 = Destination()
+    dest2.name = "Shahara"
+    dest2.desc = " sunny light "
+    dest2.price = 696
+
+    dest3 = Destination()
+    dest3.name = "red bridge"
+    dest3.desc = " hanging pridge"
+    dest3.price = 969
+    return render(request,'index.html',{'dest1': dest1 ,'dest2' : dest2 ,'dest3':dest3 })
