2082b0057b5ec24785fa5aff31dbc2436f07211f
ansalr <ansalr.email@gmail.com>
2023-03-15 18:23:01 +0530

    dynamic special offer

--- firstproject/travello/views.py
+++ firstproject/travello/views.py
@@ -10,18 +10,21 @@ def index (request):
     dest1.desc = " beach resort"
     dest1.img = "destination_1.jpg"    
     dest1.price = 666
+    dest1.offer = True
 
     dest2 = Destination()
     dest2.name = "Shahara"
     dest2.desc = " sunny light "
     dest2.img = "destination_2.jpg" 
     dest2.price = 696
+    dest2.offer = False
 
     dest3 = Destination()
     dest3.name = "red bridge"
     dest3.desc = " hanging pridge"
     dest3.img = "destination_3.jpg" 
     dest3.price = 969
+    dest3.offer = True
 
     dests = [dest1,dest2,dest3]
     return render(request,'index.html',{'dests': dests })
