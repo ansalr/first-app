cd9863b3424e8c463139315371c812a172d5aa22
Ansal <65532075+ansalr@users.noreply.github.com>
2023-03-15 18:12:08 +0530

    pull req trail

--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+#hi ansal just pull
+
 urlpatterns = [
     
     path('',include('travello.urls')),
